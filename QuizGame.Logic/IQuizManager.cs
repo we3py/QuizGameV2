27852ab1e14a3517a4f2cf7810753cca057db83e
we3py <CRV374@motorolasolutions.com>
2022-06-21 20:21:07 +0200

    Create interface to manage Quiz

---
+++ QuizGame.Logic/IQuizManager.cs
@@ -0,0 +1,14 @@
+﻿using QuizGame.Data;
+
+namespace QuizGame.Logic
+{
+    public interface IQuizManager
+    {
+        string[] Answers { get; set; }
+        int Highscore { get; set; }
+        Question[] InGameQuestions { get; set; }
+
+        void CalculateHighscore();
+        void SetUpAnswer(int answerIndex, string answer);
+    }
+}
