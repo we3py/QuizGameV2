521cfd8350967bc7d7b15a121ff4034fc5003baf
Piotr Bujak <piotr.bujak@motorolasolutions.com>
2022-06-22 11:55:11 +0200

    Add QuestionFactory tests

--- QuizGame.Test/Factories/QuestionFactoryTests.cs
+++ QuizGame.Test/Factories/QuestionFactoryTests.cs
@@ -1,7 +1,4 @@
-﻿using NSubstitute;
-using NUnit.Framework;
-using QuizGame.Data.Factories;
-using System;
+﻿using QuizGame.Data.Factories;
 
 namespace QuizGame.Test.Factories
 {
@@ -22,22 +19,71 @@ namespace QuizGame.Test.Factories
         }
 
         [Test]
-        public void GetNew_StateUnderTest_ExpectedBehavior()
+        public void GetNew_NullQueryValue_NotNullValue()
         {
             // Arrange
             var factory = this.CreateFactory();
             string query = null;
-            string correctAnswer = null;
-            string[] answers = null;
+            string correctAnswer = "A";
+            string[] answers = { "AA", "AB", "AC", "AD" };
 
             // Act
-            var result = factory.GetNew(
-                query,
-                correctAnswer,
-                answers);
+            var result = factory.GetNew(query, correctAnswer, answers);
 
             // Assert
-            Assert.Fail();
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        public void GetNew_TooFewAnswers_IndexOutOfRangeException()
+        {
+            // Arrange
+            var factory = this.CreateFactory();
+            string query = "Question";
+            string correctAnswer = "A";
+            string[] answers = { "AA", "AB", "AC" };
+
+            // Act          
+
+            // Assert
+            Assert.Throws<IndexOutOfRangeException>(() =>
+            {
+                factory.GetNew(query, correctAnswer, answers);
+            });
+        }
+        [Test]
+        public void GetNew_TooManyAnswers_NotNullValue()
+        {
+            // Arrange
+            var factory = this.CreateFactory();
+            string query = "Question";
+            string correctAnswer = "A";
+            string[] answers = { "AA", "AB", "AC", "AD", "AE" };
+
+            // Act          
+            var result = factory.GetNew(query, correctAnswer, answers);
+            // Assert
+            Assert.That(result, Is.Not.Null);
+        }
+        [Test]
+        public void GetNew_NullCorrectAnswerValue_NotNullValue()
+        {
+            // Arrange
+            var factory = this.CreateFactory();
+            string query = "question";
+            string correctAnswer = null;
+            string[] answers = { "AA", "AB", "AC", "AD" };
+
+            // Act
+            var result = factory.GetNew(query, correctAnswer, answers);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
         }
     }
+
 }
+/* Assert.Throws<NullReferenceException>(() =>
+            {
+                factory.GetNew(query, correctAnswer, answers);
+            });*/
