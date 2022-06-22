e441594f4ae03b7e4c8a54f69cea29e81b865c63
grusus <grusus@gmail.com>
2022-06-22 15:14:38 +0200

    Tests code refactor

--- QuizGame.Test/Validators/QuestionValidationTests.cs
+++ QuizGame.Test/Validators/QuestionValidationTests.cs
@@ -6,23 +6,16 @@ namespace QuizGame.Test.Validators
     [TestFixture]
     public class QuestionValidationTests
     {
-
-
-
-
         [Test]
         public void Validate_NullDataToValidate_NullReferenceException()
         {
             // Arrange
 
-            CsvReader dataToValidate = null;
-
             // Act
-
 
             // Assert
             Assert.Throws<NullReferenceException>(
-      () => { QuestionValidation.Validate(dataToValidate); });
+            () => QuestionValidation.Validate(null));
         }
     }
 }
