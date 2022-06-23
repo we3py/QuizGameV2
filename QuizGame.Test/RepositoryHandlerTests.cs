e8ec0771b2e9b82dd299895170ae894703b6bfb6
Artgelika <artgelika.com>
2022-06-23 14:52:34 +0200

    Add Repository Handler Test - mock a database

--- QuizGame.Test/RepositoryHandlerTests.cs
+++ QuizGame.Test/RepositoryHandlerTests.cs
@@ -1,96 +1,37 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using QuizGame.Data;
+using Moq;
+using QuizGame.Data.DAL;
+using NSubstitute;
 
 namespace QuizGame.Test
 {
     [TestFixture]
     public class RepositoryHandlerTests
     {
-        [SetUp]
-        public void SetUp()
-        {
-        }
-/*
-        private static RepositoryHandler CreateRepositoryHandler()
-        {
-            return new RepositoryHandler();
-        }
-
         [Test]
-        public void AddQuestion_QuestionIsNull_ThrowNullReferenceException()
+        public void CreateRepository_savesABlogViaContext()
         {
             // Arrange
-            var repositoryHandler = CreateRepositoryHandler();
+            var mockSet = new Mock<DbSet<Question>>();
+            var mockContext = new Mock<QuizGameContext>();
+
+            mockContext.Setup(s => s.Set<Question>()).Returns(mockSet.Object);
+            var questionRepository = new RepositoryHandler(mockContext.Object);
 
             // Act
-
-            // Assert
-            Assert.Throws<NullReferenceException>(() => repositoryHandler.AddQuestion(null));
-        }
-
-        [Test]
-        public void GetExistingQuestions_GetListOfQuestions_CorrectListOfQuestions()
-        {
-            // Arrange
-            var repositoryHandler = CreateRepositoryHandler();
-            var context = new QuizGameContext();
-            var expected = context.Questions.ToList();
-
-            // Act
-            var result = repositoryHandler.GetExistingQuestions();
-
-            // Assert
-            Assert.That(result, Has.Count.EqualTo(expected.Count));
-            for (int i = 0; i < expected.Count; i++)
+            questionRepository.AddQuestion(new Question()
             {
-                Assert.Multiple(() =>
-                {
-                    Assert.That(expected[i].QuestionId, Is.EqualTo(result[i].QuestionId));
-                    Assert.That(expected[i].Query, Is.EqualTo(result[i].Query));
-                    Assert.That(expected[i].AnswerA, Is.EqualTo(result[i].AnswerA));
-                    Assert.That(expected[i].AnswerB, Is.EqualTo(result[i].AnswerB));
-                    Assert.That(expected[i].AnswerC, Is.EqualTo(result[i].AnswerC));
-                    Assert.That(expected[i].AnswerD, Is.EqualTo(result[i].AnswerD));
-                    Assert.That(expected[i].CorrectAnswer, Is.EqualTo(result[i].CorrectAnswer));
-                });
-            }
+                Query = "Question1",
+                AnswerA = "A1",
+                AnswerB = "A2",
+                AnswerC = "A3",
+                AnswerD = "A4",
+                CorrectAnswer = "A"
+            });
+
+            Question question = questionRepository.GetExistingQuestions()[0];
+            Assert.That(question, Is.Not.Null);
         }
-
-        [Test]
-        public void AddHighscore_UserNameNull_DbUpdateException()
-        {
-            // Arrange
-            var repositoryHandler = CreateRepositoryHandler();
-            const int score = 0;
-
-            // Act
-
-            // Assert
-            Assert.Throws<DbUpdateException>(() => repositoryHandler.AddHighscore(null, score));
-        }
-
-        [Test]
-        public void GetHighscores_GetListOfHighscores_CorrectListOfHighScores()
-        {
-            // Arrange
-            var repositoryHandler = CreateRepositoryHandler();
-            var context = new QuizGameContext();
-            var expected = context.Highscores.ToList();
-
-            // Act
-            var result = repositoryHandler.GetHighscores();
-
-            // Assert
-            Assert.That(result, Has.Count.EqualTo(expected.Count));
-            for (int i = 0; i < expected.Count; i++)
-            {
-                Assert.That(expected[i].HighscoreId, Is.EqualTo(result[i].HighscoreId));
-                Assert.Multiple(() =>
-                {
-                    Assert.That(expected[i].UserName, Is.EqualTo(result[i].UserName));
-                    Assert.That(expected[i].Score, Is.EqualTo(result[i].Score));
-                });
-            }
-        }*/
     }
 }
